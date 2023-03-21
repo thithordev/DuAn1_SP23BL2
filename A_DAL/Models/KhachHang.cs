5f8712df9af73ad36db1c3048d75b42751618151
Vdnxdivv <namvdph29235@fpt.edu.vn>
2023-03-21 16:00:58 +0700

    nv + kh2

--- A_DAL/Models/KhachHang.cs
+++ A_DAL/Models/KhachHang.cs
@@ -8,5 +8,12 @@ namespace A_DAL.Models
 {
     internal class KhachHang
     {
+        public Guid Id { get; set; }
+        public string hoTen { get; set; }
+        public string CCCD { get; set; }
+        public string SDT { get; set; }
+        public bool gioiTinh { get; set; }
+        public string diaChi { get; set; }
+        public string ghiChu { get; set; }
     }
 }
--- A_DAL/Models/NhanVien.cs
+++ A_DAL/Models/NhanVien.cs
@@ -8,5 +8,17 @@ namespace A_DAL.Models
 {
     internal class NhanVien
     {
+        public Guid Id { get; set; }
+        public Guid IDChucVu { get; set; }
+        public Guid IDCapTren { get; set; }
+        public string ten { get; set; }
+        public string SDT { get; set; }
+        public string diaChi { get; set; }
+        public string gioiTinh { get; set; }
+        public DateTime ngSinh { get; set; }
+        public int trangThai { get; set; }
+        public string Password { get; set; }
+        public int Luong { get; set; }
+        public string? note { get; set; }
     }
 }
