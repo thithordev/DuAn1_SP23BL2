ce56d49cd5644415aca037def90ac7687a731d12
trungsamset0 <trungsamset0@gmail.com>
2023-03-22 20:26:47 +0700

    UPDATE loi

--- C_GUI/Program.cs
+++ C_GUI/Program.cs
@@ -12,11 +12,6 @@ namespace C_GUI
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
-<<<<<<< HEAD
-=======
-            //TestTrung123
-            //:D
->>>>>>> 87d8b81ad0fd5a6332d4bd72fff6b08461b42b5b
         }
     }
 }
