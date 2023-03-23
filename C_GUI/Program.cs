7cb4d4864f0c4cf1a52851fd42cc086675cf5472
Vdnxdivv <namvdph29235@fpt.edu.vn>
2023-03-23 14:55:44 +0700

    Revert "10"
    This reverts commit 15747288aec8f9a8004d04450300ce3a1fe03197.

--- C_GUI/Program.cs
+++ C_GUI/Program.cs
@@ -13,14 +13,7 @@ namespace C_GUI
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-<<<<<<< HEAD
             Application.Run(new FrmKhung());
-=======
-            Application.Run(new FrmDangNhap());
-            //lmao
-            //xD
-
->>>>>>> 10
         }
     }
 }
