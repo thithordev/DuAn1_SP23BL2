33af98fb0ff96f9de27a5588903890e502654d3e
trungsamset0 <trungsamset0@gmail.com>
2023-03-24 21:17:46 +0700

    xong Repos và IRepos Trung

--- A_DAL/IRepositories/ILoaiPhongRepository.cs
+++ A_DAL/IRepositories/ILoaiPhongRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using A_DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace A_DAL.IRepositories
 {
     public interface ILoaiPhongRepository
     {
+        bool Add(LoaiPhong loaiPhong);
+        bool Update(LoaiPhong loaiPhong);
+        bool Delete(LoaiPhong loaiPhong);
+        List<LoaiPhong> GetAll();
+        LoaiPhong GetByID(Guid id);
     }
 }
--- A_DAL/IRepositories/IPhongRepository.cs
+++ A_DAL/IRepositories/IPhongRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using A_DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace A_DAL.IRepositories
 {
     public interface IPhongRepository
     {
+        bool Add(Phong phong);
+        bool Update(Phong phong);
+        bool Delete(Phong phong);
+        List<Phong> GetAll();
+        Phong GetByID(Guid id);
     }
 }
