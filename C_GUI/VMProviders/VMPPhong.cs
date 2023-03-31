456ce1cac691f416de4bf27d3a958d7c5c0f8479
thithordev <thindph28966@fpt.edu.vn>
2023-03-31 21:04:17 +0700

    chỉnh phieu dat phong

---
+++ C_GUI/VMProviders/VMPPhong.cs
@@ -0,0 +1,29 @@
+﻿using B_BUS.Services;
+using B_BUS.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_GUI.VMProviders
+{
+    public class VMPPhong : BaseVMP
+    {
+        private static VMPPhong? _ins;
+        public static VMPPhong Ins
+        {
+            get { if (_ins == null) _ins = new VMPPhong(); return _ins; }
+            set { _ins = value; }
+        }
+
+        public PhongService service { get; set; }
+        public PhongViewModel VM { get; set; }
+
+        public VMPPhong()
+        {
+            service = new PhongService();
+            VM = new PhongViewModel();
+        }
+    }
+}
