63d015bb76a2b07e35e16e4d0cb4d10622739930
04449819 <kiennvph18558@fpt.edu.vn>
2023-03-31 19:01:46 +0700

    a

--- C_GUI/Views/FrmDichVu.cs
+++ C_GUI/Views/FrmDichVu.cs
@@ -2,22 +2,24 @@
 using B_BUS.IServices;
 using B_BUS.Services;
 using B_BUS.ViewModels;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace C_GUI.Views
 {
     public partial class FrmDichVu : Form
     {
-        DichVu doiTuong;
         IDichVuService _dichVu;
         public FrmDichVu()
         {
             InitializeComponent();
             _dichVu = new DichVuService();
-            doiTuong = new DichVu();
-            loadDV();
+            loadDV(_dichVu.GetAll());
         }
-        void loadDV()
+        void loadDV(List<DichVuViewModel> ds)
         {
+            //txtTimKiem.Text = "Tìm kiếm theo tên";
+            //txtTimKiem.Font = 
             DataGridViewButtonColumn btnSua = new DataGridViewButtonColumn();
             btnSua.UseColumnTextForButtonValue = true;
             btnSua.Text = "Sửa";
@@ -29,9 +31,9 @@ namespace C_GUI.Views
             int index = 1;
             dgvDichVu.ColumnCount = 4;
             dgvDichVu.Columns[0].Name = "STT";
-            dgvDichVu.Columns[1].Name = "Tên san phẩm";
+            dgvDichVu.Columns[1].Name = "Tên sản phẩm";
             dgvDichVu.Columns[1].Width = 220;
-            dgvDichVu.Columns[2].Name = "Gía";
+            dgvDichVu.Columns[2].Name = "Giá";
             dgvDichVu.Columns[2].Width = 200;
             dgvDichVu.Columns[3].Name = "ID";
             dgvDichVu.Columns[3].Visible = false;
@@ -39,17 +41,13 @@ namespace C_GUI.Views
             dgvDichVu.Rows.Clear();
             dgvDichVu.Columns.Insert(4, btnSua);
             dgvDichVu.Columns.Insert(5, btnXoa);
-            foreach (var x in  _dichVu.GetAll())
+            foreach (var x in  ds)
             {
                 dgvDichVu.Rows.Add(index++, x.Ten, x.Gia,x.IdDichVu);
             }
+            //dgvDichVu.Rows[_dichVu.GetAll().Count()].Visible = false;
         }
         //string iddv, ten, gia;
-
-        private void dgvDichVu_CellClick_1(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
         private void dgvDichVu_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
         {
             var iddv = dgvDichVu.Rows[e.RowIndex].Cells[3].Value.ToString();
@@ -65,13 +63,44 @@ namespace C_GUI.Views
                     MessageBox.Show(_dichVu.Update(dt));
                     break;
                case 5:
-                    //MessageBox.Show(_dichVu.Delete(dt));
+                    //dgvDichVu.Rows[e.RowIndex].Visible = false;
                     break;
                 default:
                     break;
             }
         }
 
+        private void txtTimKiem_Enter(object sender, EventArgs e)
+        {
+            txtTimKiem.Text = "";
+        }
 
+        public string LoaiDau(string str)
+        {
+            Regex regex = new Regex("\\p{IsCombiningDiacriticalMarks}+");
+            string temp = str.Normalize(NormalizationForm.FormD);
+            return regex.Replace(temp, String.Empty)
+                        .Replace('đ', 'd').Replace('Đ', 'D').Replace(" ","");
+        }
+
+
+        private void txtTimKiem_TextChanged(object sender, EventArgs e)
+        {
+                var ds = from x in _dichVu.GetAll()
+                         where LoaiDau(x.Ten).ToLower().Contains(LoaiDau(txtTimKiem.Text).ToLower())
+                         select x;
+                loadDV(ds.ToList()); 
+        }
+
+        private void btnThemDV_Click(object sender, EventArgs e)
+        {
+            FrmThemDV frm = new FrmThemDV();
+            frm.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            loadDV(_dichVu.GetAll());
+        }
     }
 }
---
+++ C_GUI/Views/FrmThemDV.cs
@@ -0,0 +1,42 @@
+﻿using B_BUS.IServices;
+using B_BUS.Services;
+using B_BUS.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_GUI.Views
+{
+    public partial class FrmThemDV : Form
+    {
+        IDichVuService _dichVu;
+        public FrmThemDV()
+        {
+            InitializeComponent();
+            _dichVu = new DichVuService();
+        }
+
+        private void btnHuyBo_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        public DichVuViewModel DuLieu()
+        {
+            DichVuViewModel dt = new DichVuViewModel();
+            dt.IdDichVu = Guid.NewGuid();
+            dt.Ten = txtTen.Text;
+            dt.Gia = decimal.Parse(txtGia.Text);
+            return dt;
+        }
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(_dichVu.Add(DuLieu()));
+        }
+    }
+}
