8bcf11b4cde133befb6dab393df2f74efb52bd37
thithordev <thindph28966@fpt.edu.vn>
2023-04-06 00:37:44 +0700

    add migration

---
+++ A_DAL/Migrations/20230405173732_v5.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace A_DAL.Migrations
+{
+    public partial class v5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SoGiuong",
+                table: "LoaiPhong",
+                newName: "OToiDa");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Tang",
+                table: "Phong",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tang",
+                table: "Phong");
+
+            migrationBuilder.RenameColumn(
+                name: "OToiDa",
+                table: "LoaiPhong",
+                newName: "SoGiuong");
+        }
+    }
+}
