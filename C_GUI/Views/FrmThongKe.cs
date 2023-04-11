d6518ad43726b5c2264ea9e0200fa96685704c49
trungsamset0 <trungsamset0@gmail.com>
2023-04-11 23:13:20 +0700

    Update FormThongKe

--- C_GUI/Views/FrmThongKe.cs
+++ C_GUI/Views/FrmThongKe.cs
@@ -6,8 +6,9 @@ namespace C_GUI.Views
 {
     public partial class FrmThongKe : Form
     {
-        IHoaDonService _hoaDonService;
-        IThuChiKhacService _thuChiKhacService;
+        private IHoaDonService _hoaDonService;
+        private IThuChiKhacService _thuChiKhacService;
+
         public FrmThongKe()
         {
             _hoaDonService = new HoaDonService();
@@ -16,29 +17,41 @@ namespace C_GUI.Views
             LoadHoaDon();
             LoadThuChiKhac();
         }
-        void LoadHoaDon()
-        {
-            foreach (var item in _hoaDonService.GetAll())
-            {
-                dgvTongThu.Rows.Add(item.NgayTao, item.GhiChu, item.TongTien);
-            }
-            for (int i = 0; i < _hoaDonService.GetAll().Count(); i++)
-            {
-                lbTongThu.Text += dgvTongThu.Rows[i].Cells[2].Value;
-            }
 
-        }
-        void LoadThuChiKhac()
+        private void LoadHoaDon()
         {
-            foreach (var item in _thuChiKhacService.GetAll())
+            decimal? tong = 0;
+            if (_hoaDonService.GetAll().Count > 0)
             {
-                dgvTongChi.Rows.Add(item.NgayTao, item.NoiDung, item.SoTien);
+                foreach (var item in _hoaDonService.GetAll())
+                {
+                    dgvTongThu.Rows.Add(item.NgayTao, item.GhiChu, item.TongTien);
+                }
+                foreach (var item in _hoaDonService.GetAll())
+                {
+
+                    tong += item.TongTien;
+                }
             }
-            for (int i = 0; i < _hoaDonService.GetAll().Count(); i++)
-            {
-                lbTongChi.Text += dgvTongChi.Rows[i].Cells[2].Value;
-            }
+            tong = 10000; // test
+            lbTongThu.Text = tong.ToString();
         }
 
+        private void LoadThuChiKhac()
+        {
+            decimal? tong = 0;
+            if (_thuChiKhacService.GetAll().Count > 0)
+            {
+                foreach (var item in _thuChiKhacService.GetAll())
+                {
+                    dgvTongChi.Rows.Add(item.NgayTao, item.NoiDung, item.SoTien);
+                }
+                foreach (var item in _thuChiKhacService.GetAll())
+                {
+                    tong += item.SoTien;
+                }
+            }
+            lbTongChi.Text = tong.ToString();
+        }
     }
 }
